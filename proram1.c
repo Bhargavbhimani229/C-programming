a48ab622c6f6be3a47ce8dd9a81cdca3d5c7d480
Bhargavbhimani229 <bhargavbhimani229@gmail.com>
2024-07-11 09:44:19 +0530

    basic C

---
+++ proram1.c
@@ -0,0 +1,115 @@
+// program 1.
+
+// #include<stdio.h>
+// void main(){
+//   printf("Hello World!");
+//   printf("\nMy Name Is Bhargav");
+// }
+
+// program 2.
+// #include<stdio.h>
+
+// int main(){
+
+//   printf("My Name : Bhimani  Bhargav");
+
+//   printf("\nLocation: Navsari,Gujrat"); 
+
+//   printf("\nContact Number: 79905 80584"); 
+
+// }
+
+// program 3.
+// #include<stdio.h>
+// #include<conio.h>
+// void main(){
+
+//   printf("*");
+//   printf("\n* *");
+//   printf("\n* * *");
+//   printf("\n* * * *");
+//   printf("\n* * * * *");
+  
+// }
+
+// Lab Work #1.2
+
+// 1.
+// #include<stdio.h>
+// void main(){
+//  printf("My Name : Bhimani  Bhargav");
+//  printf("\nAge: 21");
+//  printf("\nSchool: Nilkanth vidhyalay");
+
+// }
+
+// 2.
+// #include<stdio.h>
+// void main(){
+// printf("- - - - - - - -");
+// printf("\n|             |");
+// printf("\nR             |");
+// printf("\nN             |");
+// printf("\nW             |");
+// printf("\n|             |");
+// printf("\n- - - - - - - -");
+
+// }
+
+// 3.
+// #include<stdint.h>
+// void main(){
+//   printf("*");
+//   printf("\n* *");
+//   printf("\n* * *");
+//   printf("\n* *");
+//   printf("\n*");
+// }
+
+// 4.
+
+// #include<stdint.h>
+// void main(){
+//   printf(" *");
+//   printf("\n *");
+//   printf("\n *");
+//   printf("\n *     * *       *");
+//   printf("\n *    *   *     *");
+//   printf("\n *   *     *   *");
+//   printf("\n *  *       * *");
+//   printf("\n * *");
+//   printf("\n *");
+// }
+
+// 5.
+
+// #include<stdio.h>
+// void main(){
+//   printf("* * * * * *");
+//   printf("\n*         *");
+//   printf("\n*         *");
+//   printf("\n*         *");
+//   printf("\n*         *");
+//   printf("\n* * * * * *");
+// }
+
+// #include<stdio.h>
+// void main(){
+//  printf(" ******* \n");
+//  printf("  ***** \n");
+//  printf("   ***  \n");
+//  printf("    *   \n");
+  
+// }
+
+
+// #include<stdio.h>
+// void main(){
+//   printf("* * * * * * * * * \n");
+//   printf("* B H A R G A V * \n");
+//   printf("* * * * * * * * * \n");
+// }
+
+
+  
+
